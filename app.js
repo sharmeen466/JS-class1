d2c88406d3b65b2b16be0a4cb5d6b19a0729c95c
sharmeen466 <sharmeennazeer2@gmail.com>
2025-02-07 00:48:19 +0500

    chap 1-4 exercises

---
+++ app.js
@@ -0,0 +1,144 @@
+//..............................chap # 1 START.................................................
+
+// Q   #   01
+// alert("Welcome")
+
+
+// Q   #   02
+// alert("Error! Please enter a valid password")
+
+
+// Q   #   03
+// alert("Welcome to JS Land \n Happy Coding!")
+
+
+// Q   #   04
+// alert("Welcome to JS Land")
+// alert("Happy coding!")
+
+
+// Q   #   05
+// console.log("Hello...I can run JS through my web browser's console")
+
+
+//..............................chap # 1 END.................................................
+
+//..............................chap # 2 START.................................................
+
+// Q   #   01
+
+// let userName;
+
+// Q   #   02
+
+// let myName = "Sharmeen"
+
+// Q   #   03
+
+// let message = "Hello World"
+// alert(message)
+
+// Q   #   04
+
+// let studentName = "Jhone Doe"
+// let age = "15 years old"
+// let skill = "certified Mobile Application Development"
+// alert(studentName)
+// alert(age)
+// alert(skill)
+
+
+// Q   #   05
+
+
+// Q   #   06
+
+// let email = "example@example.com"
+// alert("My email address is "+ email)
+
+
+// Q   #   07
+
+// let book = "“A smarter way to learn JavaScript”."
+// alert(book)
+
+
+// Q   #   08
+// document.write("Yah! I can write HTML content throgh JavaScript")
+
+
+// Q   #   09
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+//..............................chap # 2 END.................................................
+
+//..............................chap # 3 START.................................................
+
+
+// Q   #   01
+
+// let age = 23
+// alert("I am " + age + " years old")
+
+
+// Q   #   02
+
+// let visit = 10
+// alert("“You have visited this site " + visit + " times”.")
+
+
+// Q   #   03
+
+// let birthYear = 2000
+// document.write("My birth year is " + birthYear)
+
+
+// Q   #   04
+
+//    let visitorName= "John Doe"
+//    let productTitle= "T-shirt"
+//    let quantity= 5
+// document.write(visitorName +" ordered "+ quantity + productTitle +"(s) on XYZ clothing store")
+
+
+//..............................chap # 3 END.................................................
+
+//..............................chap # 4 START.................................................
+
+// Q   #   01
+
+// let studentData={
+//     studentName,
+//     age,
+//     marks,
+// }
+
+
+// Q   #   02
+
+// 5 Legal Variables
+// 1- allowed letters
+// 2- allowed $ nd _
+// 3- allowed numbers in center
+// 4- allowed numbers in the end
+// 5- aloowed in all cases but best in camel case
+
+// 5 Illegal Variables
+// 1- In starting, numbers are not allowed
+// 2- not allowed special charactors except $ nd _
+// 3- special charactors are not allowed in center
+// 3- keywords are not allowed 
+// 4- space is not allowed
+
+
+// Q   #   03
+
+// document.write ("<h1> “Rules for naming JS variables” </h1> <br>")
+// document.write ("Variables names can only contain '$' , '_' , and numbers <br>")
+// document.write ("Variables must begin with '$' , '_' , and name <br>")
+// document.write ("Variables names are 'Camel case' <br>")
+// document.write ("Variables names should not be 'keywords'")
+
+
+//..............................chap # 4 END.................................................
